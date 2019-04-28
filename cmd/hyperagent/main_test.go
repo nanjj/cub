806c4c488f1bdc939ee85c160101fa90f9b7253e
JUN JIE NAN <nanjunjie@gmail.com>
2019-04-28 15:40:46 +0800

    Removed log in main package

--- cmd/hyperagent/main_test.go
+++ cmd/hyperagent/main_test.go
@@ -1,1 +1,14 @@
-package main
+package main_test
+
+import (
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	rt := func() int {
+		os.Setenv("HYPERAGENT_RUNNER_IP", "127.0.0.1")
+		return m.Run()
+	}()
+	os.Exit(rt)
+}
