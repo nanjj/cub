9edb7c8752fe4ac4b3bae68956a6b98a35bb39a5
JUN JIE NAN <nanjunjie@gmail.com>
2019-04-28 14:30:37 +0800

    Added more tests

--- logs/span_test.go
+++ logs/span_test.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"testing"
 
+	gomock "github.com/golang/mock/gomock"
 	"github.com/nanjj/cub/logs"
 	"github.com/opentracing/opentracing-go"
 	"go.uber.org/zap"
@@ -14,27 +15,27 @@ import (
 func BenchmarkLogging(b *testing.B) {
 	b.Run("ZapLogSpan", func(b *testing.B) {
 		b.ReportAllocs()
-		sp, ctx := logs.StartSpanFromContext(context.Background(), b.Name())
-		defer sp.Finish()
+		sp1, ctx := logs.StartSpanFromContext(context.Background(), b.Name())
+		defer sp1.Finish()
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
 			func(ctx context.Context) {
-				sp, ctx := logs.StartSpanFromContext(ctx, "f1")
-				sp.Info("BenchmarkStartSpanFromContext")
-				defer sp.Finish()
+				sp1, ctx := logs.StartSpanFromContext(ctx, "f1")
+				sp1.Info("BenchmarkStartSpanFromContext")
+				defer sp1.Finish()
 			}(ctx)
 		}
 	})
 
 	b.Run("ZapSpanNoLog", func(b *testing.B) {
 		b.ReportAllocs()
-		sp, ctx := logs.StartSpanFromContext(context.Background(), b.Name())
-		defer sp.Finish()
+		sp1, ctx := logs.StartSpanFromContext(context.Background(), b.Name())
+		defer sp1.Finish()
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
 			func(ctx context.Context) {
-				sp, ctx := logs.StartSpanFromContext(ctx, "f1")
-				defer sp.Finish()
+				sp1, ctx := logs.StartSpanFromContext(ctx, "f1")
+				defer sp1.Finish()
 			}(ctx)
 		}
 	})
@@ -62,18 +63,18 @@ func BenchmarkLogging(b *testing.B) {
 		b.ReportAllocs()
 		var (
 			ctx context.Context
-			sp  *logs.SpanLogger
+			sp1 *logs.SpanLogger
 		)
 		name := b.Name()
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
 			if i%90 == 0 {
-				sp, ctx = logs.StartSpanFromContext(context.Background(), name)
-				defer sp.Finish()
+				sp1, ctx = logs.StartSpanFromContext(context.Background(), name)
+				defer sp1.Finish()
 			}
 			func(ctx context.Context) {
-				sp := logs.SpanFromContext(ctx)
-				sp.Info("BenchmarkStartSpanFromContext")
+				sp1 := logs.SpanFromContext(ctx)
+				sp1.Info("BenchmarkStartSpanFromContext")
 			}(ctx)
 		}
 	})
@@ -106,10 +107,10 @@ func TestContext(t *testing.T) {
 
 func TestSpanContext(t *testing.T) {
 	tracer := opentracing.GlobalTracer()
-	sp := tracer.StartSpan("TestSpanContext")
-	defer sp.Finish()
+	sp1 := tracer.StartSpan("TestSpanContext")
+	defer sp1.Finish()
 	carrier := map[string]string{}
-	err := logs.Inject(tracer, sp.Context(), carrier)
+	err := logs.Inject(tracer, sp1.Context(), carrier)
 	if err != nil {
 		t.Fatal()
 	}
@@ -122,6 +123,106 @@ func TestSpanContext(t *testing.T) {
 		t.Fatal(err)
 	}
 	ctx := logs.WithValues(nil, carrier)
-	sp, ctx = opentracing.StartSpanFromContextWithTracer(ctx, tracer, "TestInject", opentracing.FollowsFrom(sc))
-	defer sp.Finish()
+	sp1, ctx = opentracing.StartSpanFromContextWithTracer(ctx, tracer, "TestInject", opentracing.FollowsFrom(sc))
+	defer sp1.Finish()
+}
+
+func TestTracerFromContext(t *testing.T) {
+	global := opentracing.GlobalTracer()
+	defer func() { opentracing.SetGlobalTracer(global) }()
+	ctx := context.Background()
+	tr := logs.TracerFromContext(ctx)
+	if tr != global {
+		t.Fatal(global, tr)
+	}
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	sp1 := NewMockSpan(ctrl)
+	sp1.EXPECT().Tracer().Return(NewMockTracer(ctrl)).Times(1)
+	sp1.EXPECT().Finish().Times(1)
+	defer sp1.Finish()
+	ctx = opentracing.ContextWithSpan(ctx, sp1)
+	tr = logs.TracerFromContext(ctx)
+	if tr == global {
+		t.Fatal(global, tr)
+	}
+}
+
+func TestSpanFromContext(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	sp1 := NewMockSpan(ctrl)
+	ctx := context.Background()
+	ctx = opentracing.ContextWithSpan(ctx, sp1)
+	span := logs.SpanFromContext(ctx)
+	if span.Span != sp1 {
+		t.Fatal(sp1, span)
+	}
+	ctx = opentracing.ContextWithSpan(ctx, span)
+	if logs.SpanFromContext(ctx) != span {
+		t.Fatal(span)
+	}
+}
+
+func TestStartSpanFromContext(t *testing.T) {
+	global := opentracing.GlobalTracer()
+	defer func() { opentracing.SetGlobalTracer(global) }()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	tr := NewMockTracer(ctrl)
+	opentracing.SetGlobalTracer(tr)
+	sp1 := NewMockSpan(ctrl)
+	sp2 := NewMockSpan(ctrl)
+	sp3 := NewMockSpan(ctrl)
+	sp1c := NewMockSpanContext(ctrl)
+
+	gomock.InOrder(
+		tr.EXPECT().StartSpan(t.Name()).Return(sp1).Times(1),
+		sp1.EXPECT().Tracer().Return(tr).Times(1),
+		sp1.EXPECT().Context().Return(sp1c).Times(1),
+		tr.EXPECT().StartSpan("Child", gomock.Any()).Return(sp2).Times(1),
+		tr.EXPECT().StartSpan("Root").Return(sp3).Times(1),
+		sp3.EXPECT().Finish().Times(1),
+		sp2.EXPECT().Finish().Times(1),
+		sp1.EXPECT().Finish().Times(1),
+	)
+	span, ctx := logs.StartSpanFromContext(nil, t.Name())
+	if span.Span != sp1 || ctx == nil {
+		t.Fatal(sp1, span, ctx)
+	}
+	defer span.Finish()
+	span, ctx = logs.StartSpanFromContext(ctx, "Child")
+	defer span.Finish()
+	span, ctx = logs.StartSpanFromContextWithTracer(nil, tr, "Root")
+	defer span.Finish()
+}
+
+func TestStartSpanFromCarrier(t *testing.T) {
+	global := opentracing.GlobalTracer()
+	defer func() {
+		opentracing.SetGlobalTracer(global)
+	}()
+	carrier := map[string]string{
+		"uber-trace-id": "4e5fda89553c4ebe:4e5fda89553c4ebe:0:1",
+	}
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	tr := NewMockTracer(ctrl)
+	sp1 := NewMockSpan(ctrl)
+	sp1c := NewMockSpanContext(ctrl)
+	gomock.InOrder(
+		tr.EXPECT().Extract(opentracing.TextMap,
+			opentracing.TextMapCarrier(carrier)).Return(
+			sp1c, nil).Times(1),
+		tr.EXPECT().StartSpan(t.Name(), gomock.Any()).Return(sp1).Times(1),
+		sp1.EXPECT().Finish().Times(1),
+	)
+	span, ctx := logs.StartSpanFromCarrier(carrier, tr, t.Name())
+	defer span.Finish()
+	if s, ok := ctx.Value("uber-trace-id").(string); !ok || s != "4e5fda89553c4ebe:4e5fda89553c4ebe:0:1" {
+		t.Fatal(s, ok)
+	}
+	if span.Span != sp1 {
+		t.Fatal(span, sp1)
+	}
 }
